13e141f6e59d726720445391cb4275857e4ceb58
James Bremner <james@ravenspoint.com>
2018-11-14 21:47:13 -0500

    refactor

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-namespace pgrid
+namespace ptree
 {
 enum class eType
 {
@@ -69,84 +69,70 @@ public:
         return ss.str();
     }
 };
-class cContent
+class cPropertyTree
 {
 public:
     vector< cProp > myProp;
     vector< cBranch > myTree;
 
+    cPropertyTree( nana::form& fm );
+
+    /** Add category ( branch ) to the tree
+        @param[in] name of the child
+        @param[in] parent name of the parent, "theRoot" if top level
+    */
     void AddCategory( const string& name,
                       const string& parent );
 
+    /// Add property of various type
     void AddProperty( const string& name,
                       const string& value,
                       eType type,
                       const string& parent );
+
+    /// Add dropdown property
     void AddProperty( const string& name,
                       const string& value,
                       eType type,
                       const vector<string>& opts,
                       const string& parent );
 
+    /// Get property by index
     cProp& Property( int k )
     {
         return myProp[k];
     }
 
-    void Text( nana::form& fm )
-    {
-        nana::msgbox msg;
-        for( auto& p : myProp )
-            msg << p.Text() << "\n";
-        msg << "\n";
-        for( auto& b : myTree )
-        {
-            msg << b.myParent << "<<" << b.myChild << "\n";
-        }
-        msg();
-    }
+    /// Text display
+    void Text( nana::form& fm );
 
+    /// Draw
     void Draw( nana::form& fm );
 
+    /// Pop-up inputbox to change property values
     void Input( nana::form& fm, int kprop );
 
-    vector< int > FindChildren( int parent )
-    {
-        vector< int > vprop;
-        int kprop = -1;
-        for ( auto& b : myTree )
-        {
-            kprop++;
-            if( b.myParent == parent )
-                vprop.push_back( kprop );
-        }
-        return vprop;
-    }
-
 private:
-    int FindParent( const string& parent )
-    {
-        int k = -1;
-        for( auto& prop : myProp )
-        {
-            k++;
-            if( prop.myType != eType::category )
-                continue;
-            if( prop.myName == parent )
-                return k;
-        }
-        throw std::runtime_error("Cannot find parent "+ parent );
-    }
+    int FindParent( const string& parent );
+
+    vector< int > FindChildren( int parent );
 
     int FindCategoryFromY( int y );
 
 };
 
+cPropertyTree::cPropertyTree( nana::form& fm )
+{
+    //Show an inputbox when the form is clicked.
+    fm.events().mouse_down([&](const nana::arg_mouse& arg)
+    {
+        Input( fm, arg.pos.y );
+    });
+}
 
-cContent thePropertyGrid;
 
-void cContent::AddCategory( const string& name,
-                            const string& parent )
+void cPropertyTree::AddCategory( const string& name,
+                                 const string& parent )
 {
     int pa = -1;
     if( parent != "theRoot" )
@@ -159,28 +145,41 @@ void cContent::AddCategory( const string& name,
 
 }
 
-void cContent::AddProperty( const string& name,
-                            const string& value,
-                            eType type,
-                            const string& parent )
+void cPropertyTree::AddProperty( const string& name,
+                                 const string& value,
+                                 eType type,
+                                 const string& parent )
 {
     int pa = FindParent( parent );
     myProp.push_back( cProp( name,value,type,pa ));
     myTree.push_back( cBranch( pa, myProp.size()-1 ));
 }
 
-void cContent::AddProperty( const string& name,
-                            const string& value,
-                            eType type,
-                            const vector<string>& opts,
-                            const string& parent )
+void cPropertyTree::AddProperty( const string& name,
+                                 const string& value,
+                                 eType type,
+                                 const vector<string>& opts,
+                                 const string& parent )
 {
     int pa = FindParent( parent );
     myProp.push_back( cProp( name,value,opts,pa ));
     myTree.push_back( cBranch( pa, myProp.size()-1 ));
 }
 
-void cContent::Draw( nana::form& fm)
+void cPropertyTree::Text( nana::form& fm )
+{
+    nana::msgbox msg;
+    for( auto& p : myProp )
+        msg << p.Text() << "\n";
+    msg << "\n";
+    for( auto& b : myTree )
+    {
+        msg << b.myParent << "<<" << b.myChild << "\n";
+    }
+    msg();
+}
+
+void cPropertyTree::Draw( nana::form& fm)
 {
     using namespace nana;
     drawing{ fm } .draw([&](paint::graphics& graph)
@@ -198,18 +197,18 @@ void cContent::Draw( nana::form& fm)
                 graph.string({ 20, 20*row }, p.myName );
                 row++;
                 depth++;
-                for( int c : thePropertyGrid.FindChildren( kprop ) )
+                for( int c : FindChildren( kprop ) )
                 {
-                    cProp& prop = thePropertyGrid.Property( c );
+                    cProp& prop = Property( c );
                     if( prop.myType == eType::category )
                     {
                         graph.string({ 20 * depth, 20*row }, prop.myName );
                         prop.myRowOnDisplay = row;
                         row++;
                         depth++;
-                        for( int c2 : thePropertyGrid.FindChildren( c ) )
+                        for( int c2 : FindChildren( c ) )
                         {
-                            cProp& prop2 = thePropertyGrid.Property( c2 );
+                            cProp& prop2 = Property( c2 );
                             prop2.myRowOnDisplay = row;
                             graph.string({ 20 * depth, 20*row },
                                          prop2.myName + ": " + prop2.myValue );
@@ -225,7 +224,7 @@ void cContent::Draw( nana::form& fm)
     });
 }
 
-void cContent::Input( nana::form& fm, int y )
+void cPropertyTree::Input( nana::form& fm, int y )
 {
     using namespace nana;
     int kprop = FindCategoryFromY( y );
@@ -328,49 +327,72 @@ void cContent::Input( nana::form& fm, int y )
     }
 }
 
-        int cContent::FindCategoryFromY( int y )
-        {
-            int targetrow = y / 20;
-            int kprop = -1;
-            for( auto& p : myProp )
-            {
-                kprop++;
-                if( p.myRowOnDisplay == targetrow )
-                    return kprop;
-            }
-            return -1;
-        }
-
-
-        void Test( nana::form& fm )
-        {
-            thePropertyGrid.AddCategory( "Signal Processing", "theRoot" );
-            thePropertyGrid.AddCategory( "Cardiac Activity Peak", "Signal Processing" );
-            thePropertyGrid.AddProperty( "Peak Finder", "none", eType::text, "Cardiac Activity Peak" );
-            thePropertyGrid.AddProperty( "Channel", "10", eType::integer, "Cardiac Activity Peak" );
-            thePropertyGrid.AddCategory( "Cardiac Peak Filters", "Signal Processing" );
-            thePropertyGrid.AddProperty( "Polarity", "positive", eType::dropdown, { "positive", "negative" }, "Cardiac Peak Filters" );
-            thePropertyGrid.AddProperty( "Height", "0.8", eType::real, "Cardiac Peak Filters" );
-            thePropertyGrid.AddProperty( "Separation", "100", eType::integer, "Cardiac Peak Filters" );
-            thePropertyGrid.AddProperty( "Width", "50", eType::integer, "Cardiac Peak Filters" );
-            thePropertyGrid.Text( fm );
-            thePropertyGrid.Draw( fm );
-        }
-    }
-    int main()
+int cPropertyTree::FindCategoryFromY( int y )
+{
+    int targetrow = y / 20;
+    int kprop = -1;
+    for( auto& p : myProp )
     {
-        using namespace nana;
-        form fm;
-
-        pgrid::Test( fm );
-
-        fm.show();
-
-        //Show an inputbox when the form is clicked.
-        fm.events().mouse_down([&fm](const nana::arg_mouse& arg)
-        {
-            pgrid::thePropertyGrid.Input( fm, arg.pos.y );
-        });
-
-        exec();
+        kprop++;
+        if( p.myRowOnDisplay == targetrow )
+            return kprop;
     }
+    return -1;
+}
+
+int cPropertyTree::FindParent( const string& parent )
+{
+    int k = -1;
+    for( auto& prop : myProp )
+    {
+        k++;
+        if( prop.myType != eType::category )
+            continue;
+        if( prop.myName == parent )
+            return k;
+    }
+    throw std::runtime_error("Cannot find parent "+ parent );
+}
+
+vector< int > cPropertyTree::FindChildren( int parent )
+{
+    vector< int > vprop;
+    int kprop = -1;
+    for ( auto& b : myTree )
+    {
+        kprop++;
+        if( b.myParent == parent )
+            vprop.push_back( kprop );
+    }
+    return vprop;
+}
+}
+
+int main()
+{
+
+    using namespace nana;
+    form fm;
+    ptree::cPropertyTree thePropertyTree( fm );
+
+
+    thePropertyTree.AddCategory( "Signal Processing", "theRoot" );
+    thePropertyTree.AddCategory( "Cardiac Activity Peak", "Signal Processing" );
+    thePropertyTree.AddProperty( "Peak Finder", "none", ptree::eType::text, "Cardiac Activity Peak" );
+    thePropertyTree.AddProperty( "Channel", "10", ptree::eType::integer, "Cardiac Activity Peak" );
+    thePropertyTree.AddCategory( "Cardiac Peak Filters", "Signal Processing" );
+    thePropertyTree.AddProperty( "Polarity", "positive", ptree::eType::dropdown, { "positive", "negative" }, "Cardiac Peak Filters" );
+    thePropertyTree.AddProperty( "Height", "0.8", ptree::eType::real, "Cardiac Peak Filters" );
+    thePropertyTree.AddProperty( "Separation", "100", ptree::eType::integer, "Cardiac Peak Filters" );
+    thePropertyTree.AddProperty( "Width", "50", ptree::eType::integer, "Cardiac Peak Filters" );
+    thePropertyTree.AddProperty( "Minimum Slope", "-1", ptree::eType::real, "Cardiac Peak Filters" );
+    thePropertyTree.AddProperty( "Maximum Slope", "-1", ptree::eType::real, "Cardiac Peak Filters" );
+    thePropertyTree.AddProperty( "Minimum Data Range", "-1", ptree::eType::integer, "Cardiac Peak Filters" );
+    thePropertyTree.AddProperty( "Moving Average", "-1", ptree::eType::real, "Cardiac Peak Filters" );
+    //thePropertyTree.Text( fm );
+    thePropertyTree.Draw( fm );
+
+    fm.show();
+
+    exec();
+}
